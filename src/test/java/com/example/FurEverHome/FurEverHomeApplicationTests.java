da6a8e9f381288371b75018d8af8e5c2aa4f2016
valentinvale <valionsemen@gmail.com>
2023-12-02 02:33:59 +0200

    added basic test hello world api

---
+++ src/test/java/com/example/FurEverHome/FurEverHomeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FurEverHome;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FurEverHomeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
