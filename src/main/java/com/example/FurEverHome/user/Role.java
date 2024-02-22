69dbed108084f4710acdb9fb89bee262efd91bc2
valentinvale <92953402+valentinvale@users.noreply.github.com>
2024-02-22 03:49:18 +0200

    added swaggerui and implemented user details

---
+++ src/main/java/com/example/FurEverHome/user/Role.java
@@ -0,0 +1,7 @@
+package com.example.FurEverHome.user;
+
+public enum Role {
+    USER,
+    ADMIN,
+    SPECIALISED
+}
--- src/main/java/com/example/FurEverHome/user/User.java
+++ src/main/java/com/example/FurEverHome/user/User.java
@@ -1,19 +1,63 @@
 package com.example.FurEverHome.user;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
 import java.util.UUID;
 
 @Entity
 @Table(name = "_user")
-public class User {
+public class User implements UserDetails {
     @Id
     private UUID id;
     private String username;
     private String firstName;
     private String lastName;
     private String email;
-    private String passwordHash;
+    private String password;
+
+    @Enumerated(jakarta.persistence.EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
